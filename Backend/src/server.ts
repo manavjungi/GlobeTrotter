fb03cd96d46d26f134433bcee5693aba9ecaf111
ModhaKrunalVipulbhai <24002170120009@mail.ljku.edu.in>
2026-08-22 15:13:51 +0530

    Made changes to remove the .js into import

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import { env } from "./config/env";
 import { checkDatabaseConnection } from "./config/database";
-import { runPendingMigrations } from "./database/migrator.js";
+import { runPendingMigrations } from "./database/migrator";
 
 async function startServer() {
   try {
